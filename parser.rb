0fe57ff6a1ef2374d0418f39431bec679870b488
BoolLi <ll024@bucknell.edu>
2013-11-19 21:36:58 -0500

    Added functionality to handle Ieeexplore Digital Library

--- parser.rb
+++ parser.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'mechanize'
 require 'optparse'
-
+require 'rest_client'
 
 OptionParser.new do |opts|
 	opts.banner = "Usage: ruby parser.rb [options]"
@@ -14,13 +14,26 @@ OptionParser.new do |opts|
 		puts doc.search('pre').map{|line| line.text}
 	end
 	opts.on('-i', '--ieee', 'fetch BibTex from IEEE Xplore') do |a|
-		agent = Mechanize.new
-		url = ARGV[0]
-		agent.get(url) do |page|
-			download_page = page.link_with(href: "/assets/v2/html/download_citations.html").click
-			#puts download_page.form.citations-format = 'citation-only'
-			agent.click(download_page.at('.button-wrap'))
-		end
+
+		RestClient.post("http://ieeexplore.ieee.org/xpl/downloadCitations", "recordIds"=> 6665065, "citations-format" => "citation-only", "download-format" => "download-bibtex", "x" => "74", "y" => "7"){|response, request, result, &block|
+			if [301, 302, 307].include?(response.code)
+				puts response.follow_redirection(request, result, &block)
+				
+			else
+				response.return!(request, result, &block)
+			end
+		}
+		#puts response.code
+		#agent = Mechanize.new
+		#url = ARGV[0]
+		#agent.get(url) do |page|
+			#download_page = page.link_with(href: "/assets/v2/html/download_citations.html").click
+			#agent.click(download_page.at('#download-bibtex'))
+			#result = download_page.forms[0].submit
+			#puts result.mech
+		#end
+		#Mechanize won't work. Try Using basic HTML functions.
+		
 		
 		
 	end
